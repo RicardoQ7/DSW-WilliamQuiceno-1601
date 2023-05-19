6029fd8dab9243d4868276a835ab73b4523f0da1
RicardoQ7 <125997879+RicardoQ7@users.noreply.github.com>
2023-05-19 05:13:58 -0500

    agregando preguntas al cuestionario

--- fase 4/js/script.js
+++ fase 4/js/script.js
@@ -2,10 +2,10 @@ function procesarRespuestas(){   // mi función
     let total = 5               // total de preguntas
     let puntos = 0;             // inicializo los puntos en cero
 
-    let myForm = document.forms["formulario"];                 // creo una variable que apunte al formulario
+    let myForm = document.forms["formulario"];                 // creo una variable que apunte al formulario (se llama formulario)
     let respuestasCorrectas = ["c", "a", "b", "a", "b"];      // en esta variable, alojo las respuestas correctas de las 5 preguntas
 
-    for (let i =1; i <= total; i++){                // creo un bucle, donde la variable 'i' inicia en 1 y va hasta <n, sumando dde 1 en los puntos acertados
+    for (let i =1; i <= total; i++){                // el ciclo for para recorrer los elementos del arreglo, donde la variable 'i' inicia en 1 y va hasta <n, sumando de 1 en las respuestas.
         if (myForm["r" + i].value == null ||        // si el cuestionario es respondido de forma incompleta
             myForm["r" + i].value == '') {          // o el cuestionario no lo respondieron 
             alert('Favor responder todas las preguntas'); // mensaje 
