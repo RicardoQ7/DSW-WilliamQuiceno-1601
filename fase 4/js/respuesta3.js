c871eea0f755ee43f1e6f01513fa1e6247bb231b
RicardoQ7 <richard1921rq@gmail.com>
2023-05-24 08:33:52 -0500

    Fase 5
    Organizando los .js con respuestas correcctas

--- fase 4/js/respuesta3.js
+++ fase 4/js/respuesta3.js
@@ -2,7 +2,7 @@ function procesarRespuestas(){   // mi función
     let total = 5               // total de preguntas
     let puntos = 0;             // inicializo los puntos en cero
 
-    let myForm = document.forms["evaluacionBoxModel"];                 // creo una variable que apunte al formulario (se llama evaluacionBoxModel)
+    let myForm = document.forms["mediaQuery"];                 // creo una variable que apunte al formulario (se llama mediaQuery)
     let respuestasCorrectas = ["b", "c", "a", "c", "b"];      // en esta variable, alojo las respuestas correctas de las 5 preguntas
 
     for (let i =1; i <= total; i++){                // el ciclo for para recorrer los elementos del arreglo, donde la variable 'i' inicia en 1 y va hasta <n, sumando de 1 en las respuestas.
