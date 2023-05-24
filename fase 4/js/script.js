ea57160e2faa33fd24c14772df9da03b4b8e5970
RicardoQ7 <richard1921rq@gmail.com>
2023-05-24 00:29:00 -0500

    Fase 5
    Agregar cuestionario 2

--- fase 4/js/script.js
+++ fase 4/js/script.js
@@ -2,7 +2,29 @@ function procesarRespuestas(){   // mi función
     let total = 5               // total de preguntas
     let puntos = 0;             // inicializo los puntos en cero
 
-    let myForm = document.forms["formulario"];                 // creo una variable que apunte al formulario (se llama formulario)
+    let myForm = document.forms["evaluacion5G"];                 // creo una variable que apunte al formulario (se llama evaluacion5G)
+    let respuestasCorrectas = ["b", "c", "a", "c", "b"];      // en esta variable, alojo las respuestas correctas de las 5 preguntas
+
+    for (let i =1; i <= total; i++){                // el ciclo for para recorrer los elementos del arreglo, donde la variable 'i' inicia en 1 y va hasta <n, sumando de 1 en las respuestas.
+        if (myForm["r" + i].value == null ||        // si el cuestionario es respondido de forma incompleta
+            myForm["r" + i].value == '') {          // o el cuestionario no lo respondieron 
+            alert('Favor responder todas las preguntas'); // mensaje 
+            return false;
+        } else {
+            if (myForm["r" + i].value === respuestasCorrectas[i -1])
+                puntos++;
+        }
+    }
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML='Obtuviste '+puntos+' puntos de ' + total + ' posibles ';
+    return false;
+}
+
+function procesarRespuestas(){   // mi función 
+    let total = 5               // total de preguntas
+    let puntos = 0;             // inicializo los puntos en cero
+
+    let myForm = document.forms["evaluacion_selectores"];                 // creo una variable que apunte al formulario (se llama evaluacion5G)
     let respuestasCorrectas = ["b", "c", "a", "c", "b"];      // en esta variable, alojo las respuestas correctas de las 5 preguntas
 
     for (let i =1; i <= total; i++){                // el ciclo for para recorrer los elementos del arreglo, donde la variable 'i' inicia en 1 y va hasta <n, sumando de 1 en las respuestas.
