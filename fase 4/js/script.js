f114ce5d3bc415529fa68caf2c2b5f983ecdc18e
RicardoQ7 <richard1921rq@gmail.com>
2023-05-24 00:53:18 -0500

    Fase 5
    Cuestionario 3

--- fase 4/js/script.js
+++ fase 4/js/script.js
@@ -19,25 +19,3 @@ function procesarRespuestas(){   // mi función
     resultado.innerHTML='Obtuviste '+puntos+' puntos de ' + total + ' posibles ';
     return false;
 }
-
-function procesarRespuestas(){   // mi función 
-    let total = 5               // total de preguntas
-    let puntos = 0;             // inicializo los puntos en cero
-
-    let myForm = document.forms["evaluacion_selectores"];                 // creo una variable que apunte al formulario (se llama evaluacion5G)
-    let respuestasCorrectas = ["b", "c", "a", "c", "b"];      // en esta variable, alojo las respuestas correctas de las 5 preguntas
-
-    for (let i =1; i <= total; i++){                // el ciclo for para recorrer los elementos del arreglo, donde la variable 'i' inicia en 1 y va hasta <n, sumando de 1 en las respuestas.
-        if (myForm["r" + i].value == null ||        // si el cuestionario es respondido de forma incompleta
-            myForm["r" + i].value == '') {          // o el cuestionario no lo respondieron 
-            alert('Favor responder todas las preguntas'); // mensaje 
-            return false;
-        } else {
-            if (myForm["r" + i].value === respuestasCorrectas[i -1])
-                puntos++;
-        }
-    }
-    let resul = document.getElementById('resul')
-    resul.innerHTML='Obtuviste '+puntos+' puntos de ' + total + ' posibles ';
-    return false;
-}
